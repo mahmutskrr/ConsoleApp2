7b3f752e9f86fea16ef6e5659e6c2206c4c069d5
mahmutskrr <77552385+mahmutskrr@users.noreply.github.com>
2021-01-16 23:49:27 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/sepetmanager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class sepetmanager
+    {
+        //naming convention
+        //synyax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("sepete eklendi"+urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("tebrikler. sepete eklendi : " + urunAdi);
+
+        }
+    }
+}
